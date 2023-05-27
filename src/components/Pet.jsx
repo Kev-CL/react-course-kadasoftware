3e33b351ab4752ef173bb6e24ca2e894305ef708
Kevin Crespo <kevincrespolopez0206@gmail.com>
2023-05-27 12:43:09 -0600

    Mod1

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,10 +1,17 @@
 // Dependencies
 import React from 'react';
 
-export const Pet = ({ name, animal, breed }) => (
-  <div>
+export const Pet = ({ images, location, id, name, animal, breed }) => {
+  if(images.length){
+    
+  }
+  
+  return(
+    <div>
     <h1>{name}</h1>
     <h2>{animal}</h2>
     <h2>{breed}</h2>
   </div>
-);
+  );
+ 
+  };
--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,5 +1,11 @@
 // Dependencies
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+// Components
+import { Pet } from './Pet';
+
+// Custom Hook
+import { useBreedList } from '../hooks/useBreedList';
 
 // Constants
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
@@ -8,7 +14,24 @@ export const SearchParams = () => {
   const [location, setLocation] = useState('');
   const [animal, setAnimal] = useState('');
   const [breed, setBreed] = useState('');
-  const breeds = [];
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
+
+  console.log('pets: ', pets);
 
   return (
     <div className="search-params">
@@ -57,6 +80,9 @@ export const SearchParams = () => {
           </select>
         </label>
       </form>
+      {pets.map((pet) => (
+        <Pet key={pet.id} name={pet.name} animal={pet.animal} breed={pet.breed} />
+      ))}
     </div>
   );
 };
