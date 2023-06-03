9cbe1ee5b45896bff67014e0f663c8f9e1a2ed05
Kevin Crespo <kevincrespolopez0206@gmail.com>
2023-06-02 20:48:26 -0600

    ModFinal

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -2,16 +2,20 @@
 import React from 'react';
 
 export const Pet = ({ images, location, id, name, animal, breed }) => {
-  if(images.length){
-    
+  let hero = 'http://pets-images.dev-apis.com/pets/none.jpg';
+  if (images.length) {
+    hero = images[0];
   }
-  
-  return(
-    <div>
-    <h1>{name}</h1>
-    <h2>{animal}</h2>
-    <h2>{breed}</h2>
-  </div>
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
- 
-  };
+};
